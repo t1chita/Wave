d4cca707189fd302ff768020020a08e52411c753
Temur Chitashvili <temur.chitashvilii@gmail.com>
2025-02-20 17:13:27 +0400

    Initial commit

---
+++ Wave/Source/AppLayer/WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveApp.swift
+//  Wave
+//
+//  Created by Temur Chitashvili on 20.02.25.
+//
+
+import SwiftUI
+
+@main
+struct WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogInView()
+        }
+    }
+}
---
+++ Wave/Source/Modules/LogIn/LogInView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wave
+//
+//  Created by Temur Chitashvili on 20.02.25.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LogInView()
+}
